955c468bdfdd8988f7f009960956593a25ea012b
Lore Simons <lore.simons@reimagine.be>
2019-01-31 22:00:30 +0100

    FinishedNLPService_Chat

--- MicroServ.API/Controllers/LicenseController.cs
+++ MicroServ.API/Controllers/LicenseController.cs
@@ -22,33 +22,40 @@ namespace MicroServ.API.Controllers
         }
 
         [HttpGet]
-        public async Task<Response> GetVaue([FromQuery]string input)
+        public async Task<Response> GetVaue([FromQuery]string input, string localEntity)
         {
-            var department = await _nlpService.GetDepartment(input);
+            var department = "404";
+            if(input != null && localEntity == null)
+                department = await _nlpService.GetDepartment(input);
+            else if(input == null && localEntity != null)
+                department = await _nlpService.GetDepartment(localEntity);
 
             var response = new Response();
             var session = new Session();
             var data = new Data();
-            data.variable = "testVa";
+            data.variable = department;
             session.data = data;
             session.@namespace = "namespace";
             response.session = session;
             IList<Message> message = new List<Message>();
             IList<TextMessage> textMessage = new List<TextMessage>();
-            textMessage.Add(new TextMessage
-            {
-                text = department
-            });
+            // textMessage.Add(new TextMessage
+            // {
+            //     text = null
+            // });
             var config = new Config();
             config.textMessages = textMessage;
-            message.Add(new Message
-            {
-                type = "text",
-                config = config
-            });
+            // message.Add(new Message
+            // {
+            //     type = "text",
+            //     config = config
+            // });
             response.messages = message;
             var action = new Action();
-            action.nextDialogstate = "d6868da1-3908-4cd5-a9ef-3d57510fc811";
+            // action.nextDialogstate = "d6868da1-3908-4cd5-a9ef-3d57510fc811";
+            action.nextDialogstate = "9d936d74-8b2d-49bd-819f-c59823ec9b2a";
+
+            
             response.action = action;
             return response;
         }
